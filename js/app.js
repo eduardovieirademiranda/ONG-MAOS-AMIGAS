75e3d58ee1e8bdd0af52c73d55dde78258f8b0a4
Eduardo Vieira de Miranda <eduardo.mir.2005@gmail.com>
2025-10-17 17:17:13 -0300

    Atualizando páginas (index, projetos, cadastro) e criando pasta js para Atividade 3

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// app.js — Atv.3 (SPA + DOM + Storage)
+// 1) MODELOS (templates) — virá depois
+// 2) FUNÇÕES — virá depois
+// 3) NAVEGAÇÃO — virá depois
+// 4) FORMULÁRIO — virá depois
+// 5) INICIALIZAÇÃO — virá depois
